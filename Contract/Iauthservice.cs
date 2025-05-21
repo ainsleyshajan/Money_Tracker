6acb334170d5721825d7d5569834662f04687268
Adone Jose <adopam25@gmail.com>
2025-05-21 14:47:13 +0530

    Create Iauthservice.cs

---
+++ Contract/Iauthservice.cs
@@ -0,0 +1,9 @@
+﻿using MoneyTracker.Services;
+namespace MoneyTracker.Contract
+{
+    public interface Iauthservice
+    {
+        Task<string> RegisterUserAsync(string username, string email, string password);
+        Task<LoginResponse> LoginUserAsync(string username, string password);
+    }
+}
