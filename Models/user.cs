9f0a170075ec63a581a58db0196594ea0e3a27d3
Adone Jose <adopam25@gmail.com>
2025-05-21 14:47:25 +0530

    Create user.cs

---
+++ Models/user.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyTracker.Models
+{
+    public class user
+    {
+        public int userId { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
